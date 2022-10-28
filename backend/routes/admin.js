f917c59ba6431cddbaec3f8f95e5a5888c96f926
Keyur <keyur.pancholi9@gmail.com>
2022-10-28 22:01:05 +0530

    Added admin login signup

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -1,6 +1,11 @@
 const router = require('express').Router()
 const adminController = require('../controllers/admin')
+const isAdmin = require("../middleware/isAdmin")
 
-router.post('/addNewJob', adminController.addNewJob)
+router.post('/addNewJob', isAdmin, adminController.addNewJob)
+
+router.post('/login', adminController.login)
+
+router.put('/signup', adminController.signup)
 
 module.exports = router
