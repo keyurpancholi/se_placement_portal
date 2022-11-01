f3ad53bfc276b6f1d837c073e40ebe462ded00af
sakshi pandey <sakshi@sakshis-MacBook-Air.local>
2022-11-01 15:15:12 +0530

    updated sidebar

--- frontend/jobzzz/src/components/Sidebar.js
+++ frontend/jobzzz/src/components/Sidebar.js
@@ -7,12 +7,13 @@ import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import AddCircleIcon from '@mui/icons-material/AddCircle';
 import WorkIcon from '@mui/icons-material/Work';
-import { Typography } from '@mui/material';
 import AccountBoxIcon from '@mui/icons-material/AccountBox';
 import { useLocation, useNavigate } from 'react-router-dom'; 
 import Container from '@mui/material/Container'; 
 import './Sidebar.css';
 import LogoutIcon from '@mui/icons-material/Logout';
+import Box from '@mui/material/Box';
+import HomeIcon from '@mui/icons-material/Home';
 
 const drawerWidth = 240;
 
@@ -43,8 +44,25 @@ function Sidebar({children}) {
         }
      ]
 
+     const item = {
+      py: '2px',
+      px: 3,
+      color: 'rgba(255, 255, 255, 0.7)',
+      '&:hover, &:focus': {
+        bgcolor: 'rgba(255, 255, 255, 0.08)',
+      },
+    };
+    
+    const itemCategory = {
+      boxShadow: '0 -1px 0 rgb(255,255,255,0.1) inset',
+      py: 1.5,
+      px: 3,
+    };
+    
+
   return (
     <div className='root'>
+
         <Drawer
         sx={{
           width: drawerWidth,
@@ -52,37 +70,47 @@ function Sidebar({children}) {
           '& .MuiDrawer-paper': {
             width: drawerWidth,
             boxSizing: 'border-box',
-            backgroundColor:'#e1f5fe'
+            backgroundColor:'#101F33'
           },
           
         }}
         variant="permanent"
         anchor="left"
       >
-        <Typography variant="h5" 
-               sx={{fontFamily:'initial',
-               marginLeft:1,
-               marginTop:2,
-               marginBottom:2,
-               fontWeight:'bold'}}>
-                    TPO Portal
-                </Typography>
-        <Divider />
+      <List>
+        <ListItem sx={{ ...item, ...itemCategory, fontSize: 22, color: '#ffff',fontFamily:'inherit' }}>
+         TPO PORTAL
+        </ListItem>
+        <ListItem sx={{ ...item, ...itemCategory }}>
+          <ListItemIcon sx={{color:'#ffff'}}>
+            <HomeIcon />
+          </ListItemIcon>
+          <ListItemText sx={{color:'#ffff'}}>Placement</ListItemText>
+        </ListItem>
+       
+        <Divider sx={{color:'#fff'}}/>
         <List>
             {menuItems.map(item => (
-                <>
+                
                 <ListItem  
                 button 
                 key={item.text}
                 onClick={() => {navigate(item.path)}}
+                sx={{ color: '#fff', 
+                '&:hover, &:focus': {
+                  bgcolor: 'rgba(255, 255, 255, 0.08)',}  }}
                 >
-                    <ListItemIcon>{item.icon}</ListItemIcon>
+                
+                    <ListItemIcon 
+                    sx={{ color: '#fff',}}>{item.icon}</ListItemIcon>
                     <ListItemText primary={item.text}/>
                 </ListItem>
-                </>
+
             ))}
         </List>
+     </List>
       </Drawer>
+
       <br />
          <div className='page'>
             <Container
