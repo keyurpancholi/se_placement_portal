f4ecc2eab82b280530b98a9262fa7256f002d520
Keyur <keyur.pancholi9@gmail.com>
2022-11-24 10:55:57 +0530

    Made dummy changes in backend

--- backend/controllers/user-w-classes.js
+++ backend/controllers/user-w-classes.js
@@ -94,21 +94,22 @@ class Users {
     };
 
     viewJobs = (req, res, next) => {
-        Job.find()
-          .then((posts) => {
-            if (!posts) {
-              const error = new Error("No posts found");
-              error.statusCode = 404;
-              throw error;
-            }
-            res.status(200).json({ posts: posts, message: "Posts found" });
-          })
-          .catch((err) => {
-            if (!err.statusCode) {
-              err.statusCode = 500;
-            }
-            next(err);
-          });
+        // Job.find()
+        //   .then((posts) => {
+        //     if (!posts) {
+        //       const error = new Error("No posts found");
+        //       error.statusCode = 404;
+        //       throw error;
+        //     }
+        //     res.status(200).json({ posts: posts, message: "Posts found" });
+        //   })
+        //   .catch((err) => {
+        //     if (!err.statusCode) {
+        //       err.statusCode = 500;
+        //     }
+        //     next(err);
+        //   });
+        res.status(200).send(<h1>Empty jobs</h1>)
     };
 
     applyForJob = (req, res, next) => {
